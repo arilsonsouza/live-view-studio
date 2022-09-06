e29ef654cd3c10de492adf788bc72e310e4d4b85
Arilson Souza <arilsons61@gmail.com>
2022-09-06 07:43:33 -0300

    Finish light view

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -18,7 +18,44 @@ defmodule LiveViewStudioWeb.LightLive do
             <%= @brightness %>%
           </span>
         </div>
+
+        <button phx-click="off">
+          <img src="images/light-off.svg"/>
+        </button>
+
+        <button phx-click="down">
+          <img src="images/down.svg"/>
+        </button>
+
+        <button phx-click="up">
+          <img src="images/up.svg"/>
+        </button>
+
+
+        <button phx-click="on">
+          <img src="images/light-on.svg"/>
+        </button>
       </div>
     """
+  end
+
+  def handle_event("on", _, socket) do
+    socket = assign(socket, :brightness, 100)
+    {:noreply, socket}
+  end
+
+  def handle_event("off", _, socket) do
+    socket = assign(socket, :brightness, 0)
+    {:noreply, socket}
+  end
+
+  def handle_event("up", _, socket) do
+    socket = update(socket, :brightness, &(&1 + 10))
+    {:noreply, socket}
+  end
+
+  def handle_event("down", _, socket) do
+    socket = update(socket, :brightness, &(&1 - 10))
+    {:noreply, socket}
   end
 end
