6082301558b080e35a739b08471f8b72ac5bed45
Arilson Souza <arilsons61@gmail.com>
2022-09-10 11:59:25 -0300

    Add license view with dynamic form

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule LiveViewStudio.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev}
+      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev},
+      {:number, "~> 1.0.1"}
     ]
   end
 
