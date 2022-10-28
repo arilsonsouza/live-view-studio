4eef798ce93140f16a20c9e3391464877bfa49c3
Arilson Souza <arilsons61@gmail.com>
2022-10-28 16:57:34 -0300

    feat: add form create live view

---
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -0,0 +1,36 @@
+defmodule LiveViewStudioWeb.VolunteersLive do
+  use LiveViewStudioWeb, :live_view
+
+  alias LiveViewStudio.Volunteers
+  alias LiveViewStudio.Volunteers.Volunteer
+
+  def mount(_params, _session, socket) do
+    volunteers = Volunteers.list_volunteers()
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
+    socket = assign(socket, volunteers: volunteers, changeset: changeset)
+
+    {:ok, socket, temporary_assigns: [volunteers: []]}
+  end
+
+  def handle_event("save", %{"volunteer" => params}, socket) do
+    socket =
+      case Volunteers.create_volunteer(params) do
+        {:ok, volunteer} ->
+          socket =
+            update(
+              socket,
+              :volunteers,
+              fn volunteers -> [volunteer | volunteers] end
+            )
+
+          changeset = Volunteers.change_volunteer(%Volunteer{})
+          assign(socket, changeset: changeset)
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          assign(socket, changeset: changeset)
+      end
+
+    {:noreply, socket}
+  end
+end
--- lib/live_view_studio_web/router.ex
+++ lib/live_view_studio_web/router.ex
@@ -29,6 +29,7 @@ defmodule LiveViewStudioWeb.Router do
     live "/servers", ServersLive
     live "/paginate", PaginateLive
     live "/sort", SortLive
+    live "/volunteers", VolunteersLive
   end
 
   # Other scopes may use custom stacks.
